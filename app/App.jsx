f519b290cecae45b192181a4d95e17895f9fe457
Sam Roberts <samgqroberts@gmail.com>
2017-02-04 19:18:10 -0500

    make layout / style of page match original
    other than canvas contents

--- app/App.jsx
+++ app/App.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import styles from './App.scss';
 
 const App = () => (
-  <div className={styles.app}>
-    sup
+  <div>
+    <h2 className={styles.header}>Supervirus</h2>
+    <p className={styles.controls}>
+      Controls: Arrow Keys. Hit things smaller than you and avoid
+      things larger than you. Godspeed.
+    </p>
+    <div className={styles.canvasContainer}>
+      TODO put da canvas here ya
+    </div>
+    <p className={styles.bottom}>
+      Development of game Supervirus by Sylver Studios
+    </p>
   </div>
 );
 
