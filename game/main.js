622a6a6cb67abb31e16811bf2e8c753232a9427a
Sam Roberts <samgqroberts@gmail.com>
2017-02-04 20:39:59 -0500

    draw the virus over the background
    in the middle and at the right size as well ok

--- game/main.js
+++ game/main.js
@@ -5,17 +5,15 @@ const backgroundImagePath = require('../assets/petri_plaincircle_withgrid.png');
 export default function Game(canvas) {
   const canvasWidth = canvas.width;
   const canvasHeight = canvas.height;
+  const canvasMiddleX = canvasWidth / 2;
+  const canvasMiddleY = canvasHeight / 2;
 
   const context = canvas.getContext('2d');
 
-  const virusImage = new Image();
-  virusImage.onload = () => context.drawImage(virusImage, 10, 10);
-  virusImage.src = virusSpritePath;
-
   const backgroundImage = new Image();
   const backgroundImageStartingSize = 1200;
-  const backgroundImageStartingX = (canvasWidth / 2) - (backgroundImageStartingSize / 2);
-  const backgroundImageStartingY = (canvasHeight / 2) - (backgroundImageStartingSize / 2);
+  const backgroundImageStartingX = canvasMiddleX - (backgroundImageStartingSize / 2);
+  const backgroundImageStartingY = canvasMiddleY - (backgroundImageStartingSize / 2);
   backgroundImage.onload = () => (
     context.drawImage(
       backgroundImage,
@@ -26,4 +24,19 @@ export default function Game(canvas) {
     )
   );
   backgroundImage.src = backgroundImagePath;
+
+  const virusImage = new Image();
+  const virusStartingSize = 32;
+  const virusStartingX = canvasMiddleX - (virusStartingSize / 2);
+  const virusStartingY = canvasMiddleY - (virusStartingSize / 2);
+  virusImage.onload = () => (
+    context.drawImage(
+      virusImage,
+      virusStartingX,
+      virusStartingY,
+      virusStartingSize,
+      virusStartingSize,
+    )
+  );
+  virusImage.src = virusSpritePath;
 }
